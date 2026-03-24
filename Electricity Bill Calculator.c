e80cd87d7cae0acdc51e80d96de8e67499a1cb1c
AlindaAlpha <jalinda618@gmail.com>
2026-03-24 17:00:40 +0300

    Alinda Atunga Alpha

---
+++ Electricity Bill Calculator.c
@@ -0,0 +1,20 @@
+// ALINDA ATUNGA ALPHA
+//25/U/BIE/01354/PE
+#include <stdio.h>
+int main (){
+    float unitsConsumed, costPerUnit, totalElectricityBill;
+    // a) ask for user input
+    printf("Enter unitsConsumed:");
+    scanf("%f", &unitsConsumed);
+
+    printf("Enter the costPerUnit:");
+    scanf("%f", &costPerUnit);
+
+    //b) perform some calculations
+    totalElectricityBill = unitsConsumed*costPerUnit;
+
+    //c) display the results
+    printf("\n The Total  Bill is : %.2f UGX\n", totalElectricityBill);
+    
+    return 0;
+}
---
+++ Fuel Efficiency Calculator.c
@@ -0,0 +1,21 @@
+//ALINDA ATUNGA ALPHA
+//25/U/BIE/01354/PE
+#include <stdio.h>
+int main(){
+    float distanceTravelled, fuelUsed, fuelEfficiency;
+    // a) ask for uset input
+    printf("Enter distanceTravelled in kilometers:");
+    scanf("%f", &distanceTravelled);
+
+    printf("Enter fuelUsed in liters:");
+    scanf("%f", &fuelUsed);
+
+    // b) perform some calculations
+    fuelEfficiency = distanceTravelled / fuelUsed;
+
+    //C) display the results
+    printf("\n The fuelEfficiency is :%.2f km/L\n", fuelEfficiency);
+
+    return 0;
+
+}
